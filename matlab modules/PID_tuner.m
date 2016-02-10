5ea64ed80afa01d00a10d189074dc295f84f44d1
RedBullInsomnia <mr.huwok@gmail.com>
2016-02-10 21:16:45 +0100

    Managed to retrieve numerical info in matlab str2num for the win !

--- matlab modules/PID_tuner.m
+++ matlab modules/PID_tuner.m
@@ -1,30 +1,27 @@
 clear all
 
-t = tcpip('127.0.0.1', 80, 'NetworkRole', 'Server');
+t = tcpip('127.0.0.1', 80, 'NetworkRole', 'server');
 fopen(t);
 disp('Blender connected')
 
 i = 1;
 data = zeros(0,0);
-while i < 250
+while i < 2
     % Read data
     incoming = get(t, 'BytesAvailable');
     if (incoming > 0)
         data = [data, fread(t, incoming)'];
         index = find(data==13);
         if 0 == isempty(index)
-            ang = char(data(1, 1:index(1)));
-            ang = str2int(ang);
-            angle(i) = ang;
+            ang = char(data(1, 1:index(1)-1));
+            angle(i) = str2num(ang);
             data = data(1,index(1)+1:end);
-            fprintf(data)
             i = i + 1;
         end
     end
 end
 
-figure
-plot(i, angle)
+angle
 
 fclose(t);
 delete(t);
