d448c7c56ebe49e030d20d42e446e024befc595e
Hubert Woszczyk <woszczykh@gmail.com>
2016-02-15 16:37:22 +0100

    Renamed servo_pid and split it

--- matlab modules/PID_tuner.m
+++ matlab modules/PID_tuner.m
@@ -1,6 +1,7 @@
 clear all
+num_servos = 1;
 
-!"C:/Program Files/Blender Foundation/Blender/blenderplayer.exe" "C:/Users/Hwk/Repos/Thesis/servo_pid.blend" &
+!"C:/Program Files/Blender Foundation/Blender/blenderplayer.exe" "C:/Users/Hubert/Repos/Thesis/1servo_pid.blend" &
 
 t = tcpip('127.0.0.1', 80, 'NetworkRole', 'server');
 fopen(t);
@@ -8,7 +9,7 @@ disp('Blender connected')
 
 count = 0;
 data = zeros(0,0);
-while count < 4
+while count < 5
     incoming = get(t, 'BytesAvailable');
     if (incoming > 0)
         data = [data, fread(t, incoming)'];
@@ -23,10 +24,11 @@ while count < 4
     end
 end
 
-d_angle = infos(1);
-kp = infos(2);
-ki = infos(3);
-kd = infos(4);
+d_angle(1) = infos(1);
+d_angle(2) = infos(2);
+kp = infos(3);
+ki = infos(4);
+kd = infos(5);
 
 i = 0;
 while i < 1000
@@ -44,26 +46,36 @@ while i < 1000
     end
 end
 
-angle1 = angle(1:2:end);
-angle2 = angle(2:2:end);
+if num_servos == 2
+	angle1 = angle(1:2:end);
+    angle2 = angle(2:2:end);
+    figure
+    dt = 1/60;
+    x = 0:dt:((i-1)/2)*dt;
+    plot(x, angle1)
+    hold on
+    plot(get(gca, 'xlim'), [d_angle(1) d_angle(1)], 'r--');
+    str = sprintf ('Kp = %f, Ki = %f, Kd = %f', kp, ki, kd);
+    title(str)
 
-figure
-dt = 1/60;
-x = 0:dt:((i-1)/2)*dt;
-plot(x, angle1)
-hold on
-plot(get(gca, 'xlim'), [d_angle d_angle], 'r--');
-str = sprintf ('Kp = %f, Ki = %f, Kd = %f', kp, ki, kd);
-title(str)
-
-figure
-dt = 1/60;
-x = 0:dt:(i-1)*dt/2;
-plot(x, angle2)
-hold on
-plot(get(gca, 'xlim'), [d_angle d_angle], 'r--');
-str = sprintf ('Kp = %f, Ki = %f, Kd = %f', kp, ki, kd);
-title(str)
+    figure
+    dt = 1/60;
+    x = 0:dt:(i-1)*dt/2;
+    plot(x, angle2)
+    hold on
+    plot(get(gca, 'xlim'), [d_angle(2) d_angle(2)], 'r--');
+    str = sprintf ('Kp = %f, Ki = %f, Kd = %f', kp, ki, kd);
+    title(str)
+else
+    figure
+    dt = 1/60;
+    x = 0:dt:(i-1)*dt;
+    plot(x, angle)
+    hold on
+    plot(get(gca, 'xlim'), [d_angle(1) d_angle(1)], 'r--');
+    str = sprintf ('Kp = %f, Ki = %f, Kd = %f', kp, ki, kd);
+    title(str)
+end
 
 fclose(t);
 delete(t);
