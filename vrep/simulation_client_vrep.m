37f09bbd46f202ce1d883e5d8b5e226b56e56b82
Hubert Woszczyk <mr.huwok@gmail.com>
2016-03-22 23:25:33 +0100

    Interpolating to smooth movement

--- vrep/simulation_client_vrep.m
+++ vrep/simulation_client_vrep.m
@@ -27,18 +27,54 @@ handles = robot_init(vrep, clientID);
 % Retrieve center of mass
 %[~, centerZ] = vrep.simxGetFloatSignal(clientID, 'centerOfMassZ', vrep.simx_opmode_oneshot_wait);
 
+% generate standup sequence
+x = 0:.1:.3;
+xq = 0:0.01:.3;
+hips_setpoints = [degtorad(30), degtorad(-40), degtorad(-150), degtorad(-20)];
+hips = interp1(x, hips_setpoints, xq, 'spline');
+
+knees_setpoints = [0, 0, degtorad(120), degtorad(20)];
+knees = interp1(x, knees_setpoints, xq, 'spline');
+
+feet_setpoints = [degtorad(-65), degtorad(-55), degtorad(-30), degtorad(0)];
+feet = interp1(x, feet_setpoints, xq, 'spline');
+
+shoulders_setpoints = [degtorad(-70), degtorad(-100), degtorad(-75), degtorad(-30)];
+shoulders = interp1(x, shoulders_setpoints, xq, 'spline');
+
+arms_setpoints = [degtorad(-90), degtorad(-90), degtorad(-90), degtorad(-90)];
+arms = interp1(x, arms_setpoints, xq, 'linear');
+
+% display points
+figure
+subplot(5,1,1)
+plot(x, hips_setpoints, 'o', xq , hips, ':.');
+
+subplot(5,1,2)
+plot(x, knees_setpoints, 'o', xq , knees, ':.');
+
+subplot(5,1,3)
+plot(x, feet_setpoints, 'o', xq , feet, ':.');
+
+subplot(5,1,4)
+plot(x, shoulders_setpoints, 'o', xq , shoulders, ':.');
+
 t = 0;
-while true && t < 2
-    instructions = standup_prone(handles, t);
+i = 1;
+while true && t < 1
+    instructions = standup_prone(handles, i, hips, knees, feet, shoulders, arms);
     send_instructions(vrep, clientID, instructions);
     t = t + dt
+    if i < 31
+        i = i + 1;
+    end
 end
 
 % Before closing the connection to V-REP, make sure that the last command sent out had time to arrive. You can guarantee this with (for example):
 vrep.simxGetPingTime(clientID);
 
 % Now close the connection to V-REP:
-%vrep.simxStopSimulation(clientID, vrep.simx_opmode_oneshot_wait);
+vrep.simxStopSimulation(clientID, vrep.simx_opmode_oneshot_wait);
 vrep.simxFinish(clientID);
 vrep.delete(); % call the destructor!
 disp('Program ended');
--- vrep/standup_prone.m
+++ vrep/standup_prone.m
@@ -1,52 +1,18 @@
-function instructions = standup_prone(h, t)
+function instructions = standup_prone(h, i, hips, knees, feet, shoulders, arms)
 
-if t < 0.3
-    % Phase 1 Lift the trunk;
-    instructions(1,:) = [double(h.right_leg_joints(2)), degtorad(30)];
-    instructions(2,:) = [double(h.left_leg_joints(2)), degtorad(30)];
-    
-    instructions(3,:) = [double(h.right_leg_joints(6)), degtorad(-55)];
-    instructions(4,:) = [double(h.left_leg_joints(6)), degtorad(-55)];
-    
-    instructions(5,:) = [double(h.right_arm_joints(1)), degtorad(-70)];
-    instructions(6,:) = [double(h.left_arm_joints(1)), degtorad(-70)];
-    
-    instructions(7,:) = [double(h.right_arm_joints(2)), degtorad(-90)];
-    instructions(8,:) = [double(h.left_arm_joints(2)), degtorad(90)];
-elseif t < 0.5
-    % Phase 2 Faire le dos rond
-    instructions(1,:) = [double(h.right_leg_joints(2)), degtorad(-40)];
-    instructions(2,:) = [double(h.left_leg_joints(2)), degtorad(-40)];
-    
-    instructions(3,:) = [double(h.right_arm_joints(1)), degtorad(-100)];
-    instructions(4,:) = [double(h.left_arm_joints(1)), degtorad(-100)];
-elseif t < 0.6
-    % Phase 3 Bring the knees in
-    instructions(1,:) = [double(h.right_leg_joints(4)), degtorad(120)];
-    instructions(2,:) = [double(h.left_leg_joints(4)), degtorad(120)];
-    
-    instructions(3,:) = [double(h.right_leg_joints(2)), degtorad(-150)];
-    instructions(4,:) = [double(h.left_leg_joints(2)), degtorad(-150)];
-    
-    % Use arms to lift trunk up
-    instructions(5,:) = [double(h.right_arm_joints(1)), degtorad(-75)];
-    instructions(6,:) = [double(h.left_arm_joints(1)), degtorad(-75)];
-    
-    % Feet
-    instructions(7,:) = [double(h.right_leg_joints(6)), degtorad(-22)];
-    instructions(8,:) = [double(h.left_leg_joints(6)), degtorad(-22)];
- else
-    % Phase 4 Stand up
-    instructions(1,:) = [double(h.right_leg_joints(2)), degtorad(-40)];
-    instructions(2,:) = [double(h.left_leg_joints(2)), degtorad(-40)];
-    
-    % Arms
-    instructions(3,:) = [double(h.right_arm_joints(1)), degtorad(-30)];
-    instructions(4,:) = [double(h.left_arm_joints(1)), degtorad(-30)];
-    
-    instructions(5,:) = [double(h.right_leg_joints(4)), degtorad(60)];
-    instructions(6,:) = [double(h.left_leg_joints(4)), degtorad(60)];
-    
-end
+instructions(1,:) = [double(h.right_leg_joints(2)), hips(i)];
+instructions(2,:) = [double(h.left_leg_joints(2)), hips(i)];
+
+instructions(3,:) = [double(h.right_leg_joints(4)), knees(i)];
+instructions(4,:) = [double(h.left_leg_joints(4)), knees(i)];
+
+instructions(5,:) = [double(h.right_leg_joints(6)), feet(i)];
+instructions(6,:) = [double(h.left_leg_joints(6)), feet(i)];
+
+instructions(7,:) = [double(h.right_arm_joints(1)), shoulders(i)];
+instructions(8,:) = [double(h.left_arm_joints(1)), shoulders(i)];
+
+instructions(9,:) = [double(h.right_arm_joints(2)), arms(i)];
+instructions(10,:) = [double(h.left_arm_joints(2)), -arms(i)];
 
 end
