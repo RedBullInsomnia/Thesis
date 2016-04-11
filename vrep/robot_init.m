e6b76b1db5e975d58dab53607a3ff9286c09586c
Hubert Woszczyk <woszczykh@gmail.com>
2016-04-11 17:26:11 +0200

    Bootstrap for Usb2dxl
    Finished model of new robot

--- vrep/robot_init.m
+++ vrep/robot_init.m
@@ -79,8 +79,8 @@ handles.right_arm_joints = right_arm_joints;
 
 cameras = [-1, -1];
 for i = 1:2
-    [~, left_leg(i)] = vrep.simxGetObjectHandle(clientID,...
-        sprintf('camera%d', (i-1)), vrep.simx_opmode_oneshot_wait);
+    [~, cameras(i)] = vrep.simxGetObjectHandle(clientID,...
+        sprintf('head%d', (i)), vrep.simx_opmode_oneshot_wait);
 end
 
 handles.cameras = cameras;
