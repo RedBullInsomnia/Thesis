f45457847dba050e50f79b538e09ed8eb1665947
Hubert Woszczyk <woszczykh@gmail.com>
2016-04-13 21:25:26 +0200

    Evening commit
    Continué rapport
    Réussi à communiquer avec servo

--- ServoControl/ServoControl2/main.cpp
+++ ServoControl/ServoControl2/main.cpp
@@ -14,6 +14,7 @@ int main(void)
 {
 	char input[MAX_IN_CHAR];
 	int port_num = 3, baud_rate = 57600, input_len, num_param;
+	int baudrates[] = {9600, 19200, 57600, 115200, 200000, 250000, 400000, 500000, 1000000 };
 
     // Initialization
 	if (dxl_initialize(port_num, baud_rate) == 0)
@@ -30,6 +31,19 @@ int main(void)
 
     while (true)
     {
-
+		for (size_t i = 0; i < 9; ++i)
+		{
+			if (dxl_change_baudrate(baudrates[i]) == 0)
+			{
+				printf("Failed to change baudrate!\n");
+				continue;
+			}
+			printf("\n Changed baudrate to [baud num : %d]\n\n", baudrates[i]);
+			dxl_ping(1);
+		}
+		break;
     }
+
+	dxl_terminate();
+	return 0;
 }
